e261b6050f1c737ee212170eaf56aeacf121ad94
Felipe Augusto <augusto_felipe@icloud.com>
2022-08-02 12:49:09 -0300

    Gesture Code

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
-
-
+    
+    @objc func changePic() {
+        
+        if (imageView.image == UIImage(named: "Dire-Straits1")) {
+            imageView.image = UIImage(named: "dire_straits2")
+            myLabel.text = "Mark Knopfler"
+        } else {
+            imageView.image = UIImage(named: "Dire-Straits1")
+            myLabel.text = "Dire Straits"
+        }
+    }
 }
 
